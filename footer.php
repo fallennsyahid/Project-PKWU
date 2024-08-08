862f2ea88976a0d68a26ff4dfa0f8a9bc4b180cc
fallennsyahid <umarusyahidmasudi1@gmail.com>
2024-08-08 17:49:58 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,29 @@
+<div id="footer-bottom">
+    <div class="container">
+      <hr class="m-0">
+      <div class="row mt-3">
+        <div class="col-md-6 copyright">
+          <p class="secondary-font">© 2023 Waggy. All rights reserved.</p>
+        </div>
+        <div class="col-md-6 text-md-end">
+          <p class="secondary-font">Free HTML Template by <a href="https://templatesjungle.com/" target="_blank"
+              class="text-decoration-underline fw-bold text-black-50"> TemplatesJungle</a> </p>
+          <p class="secondary-font">Distributed by <a href="https://themewagon.com/" target="_blank"
+              class="text-decoration-underline fw-bold text-black-50"> ThemeWagon</a> </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <script src="js/jquery-1.11.0.min.js"></script>
+  <script src="js/swiper-bundle.js"></script>
+  <script src="js/boostrap-bundle.js"
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+    crossorigin="anonymous"></script>
+  <script src="js/plugins.js"></script>
+  <script src="js/script.js"></script>
+  <script src="js/icon.js"></script>
+</body>
+
+</html>
